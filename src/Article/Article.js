70e51ba349fb688b207335295c08493c25e7efd9
Stephanie Kane <stephaniekane@Stephanies-MacBook-Pro.local>
2021-01-11 13:24:39 -0500

    styled articles in reader

--- src/Article/Article.js
+++ src/Article/Article.js
@@ -1,12 +1,20 @@
 import React from 'react';
-
+import './Article.css';
 
 class Article extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    
+    handleClick() {
+        this.props.onClick(this.props.title);
+    }
 
 
     render(){
         return(
-            <div>
+            <div onClick={this.handleClick} className="article">
                 <h2 className="articleTitle">{this.props.title}</h2>
                 <p className="articleText">{this.props.body}</p>
             </div>
--- src/Reader/Reader.js
+++ src/Reader/Reader.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import Articles from '../articles.json';
 import Article from '../Article/Article';
+import './Reader.css';
 
 class Reader extends React.Component{
     constructor(props){
         super(props);
         this.state = {
             articles : Articles,
+            selected : null
         }
+        this.clickArticle = this.clickArticle.bind(this);
+    }
+
+    clickArticle(article){
+        this.setState({
+            selected : article
+        });
     }
 
     render(){
+
         return(
-            <div>
+            <div id='reader'>
                 {Object.entries(this.state.articles).map(([key, value])=>(
                         <Article
+                            key={value.title}
                             title={value.title}
                             body={value.body}
                             image={value.imgsrc}
+                            tags={value.tags}
+                            onClick={this.clickArticle}
                         />
                     )
                 )}
