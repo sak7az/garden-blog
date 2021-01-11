8610ee7eb1eb441d9bdce3039300c1387f197a68
Stephanie Kane <stephaniekane@Stephanies-MacBook-Pro.local>
2021-01-11 15:58:23 -0500

    basic formatting for single article

--- src/Article/Article.js
+++ src/Article/Article.js
@@ -6,9 +6,10 @@ class Article extends React.Component{
     render(){
         return (
             <div id="article">
+                <button onClick={this.props.backClick}>Back</button>
                 <h3>{this.props.article.title}</h3>
                 <p>{this.props.article.body}</p>
-                <button onClick={this.props.backClick}>Click to return to Articles</button>
+                <button onClick={this.props.backClick}>Back</button>
             </div>
         )
     }
