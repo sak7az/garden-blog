e9f3496f1294483dd5168f61435eaa907faaf04a
Stephanie Kane <stephaniekane@Stephanies-MacBook-Pro.local>
2021-01-11 14:19:59 -0500

    color scheme added

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,7 +6,14 @@ class Header extends React.Component{
     render(){
         return (
             <div id="header">
-                <h1> Tomatoes </h1>
+                <h1> 
+                    <span>
+                        Photo by 
+                            <a href="https://unsplash.com/@markusspiske?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Markus Spiske</a> 
+                        on 
+                            <a href="https://unsplash.com/s/photos/garden?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a>
+                    </span> 
+                </h1>
             </div>
         )
     }
