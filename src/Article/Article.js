ed3d132a92bb491815af2d7e4771d14279b5d858
Stephanie Kane <stephaniekane@Stephanies-MacBook-Pro.local>
2021-01-11 15:53:54 -0500

    Reader passes article object to Article when selected, Article uses object to render text

--- src/Article/Article.js
+++ src/Article/Article.js
@@ -6,8 +6,8 @@ class Article extends React.Component{
     render(){
         return (
             <div id="article">
-                <h3>Title</h3>
-                <p>content</p>
+                <h3>{this.props.article.title}</h3>
+                <p>{this.props.article.body}</p>
                 <button onClick={this.props.backClick}>Click to return to Articles</button>
             </div>
         )
