75e3eebe464c29e544990ef440526f5a1c8cd19b
Stephanie Kane <stephaniekane@Stephanies-MacBook-Pro.local>
2021-01-11 15:32:00 -0500

    background image added and styled

--- src/App.js
+++ src/App.js
@@ -12,3 +12,6 @@ function App() {
 }
 
 export default App;
+
+
+//      <footer><span>Photo by <a href="https://unsplash.com/@monstercritic?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Sergey Shmidt</a> on <a href="https://unsplash.com/s/photos/flowers?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span></footer>
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,14 +6,11 @@ class Header extends React.Component{
     render(){
         return (
             <div id="header">
-                <h1> 
-                    <span>
-                        Photo by 
-                            <a href="https://unsplash.com/@markusspiske?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Markus Spiske</a> 
-                        on 
-                            <a href="https://unsplash.com/s/photos/garden?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a>
-                    </span> 
-                </h1>
+                <div id="overlay">
+                    <h1> 
+                        Clever Title goes Here
+                    </h1>
+                </div>
             </div>
         )
     }
