7cd2ffd6715ca217a65a0e92e6cbec2d9394d0c3
Stephanie Kane <stephaniekane@Stephanies-MacBook-Pro.local>
2021-01-13 14:58:01 -0500

    added display state to Body

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -3,11 +3,24 @@ import Menu from '../Menu/SideBar';
 import Reader from '../Reader/Reader';
 
 class Body extends React.Component{
+    constructor(props){
+        super(props);
+        this.selectMenuItem = this.selectMenuItem.bind(this);
+        this.state = {
+            display : "home"
+        }
+    }
+
+    selectMenuItem(item){
+        
+    }
 
     render(){
         return (
             <div>
-                <Menu />
+                <Menu 
+                    selector={this.selectMenuItem}
+                />
                 <Reader />
             </div>
         )
