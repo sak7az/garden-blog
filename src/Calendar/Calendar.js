c48fb17bef8a94383f7694a3bae83d229d2d4b45
Stephanie Kane <stephaniekane@Stephanies-MacBook-Pro.local>
2021-01-13 15:33:02 -0500

    initialized stand-in components for About, Cal, Contact

---
+++ src/Calendar/Calendar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Calendar extends React.Component{
+
+    render(){
+        return ( 
+            <div>
+                <h2>Calendar Coming Soon...</h2>
+            </div>
+        )
+    }
+}
+
+export default Calendar;
